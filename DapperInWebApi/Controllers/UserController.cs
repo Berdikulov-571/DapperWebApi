b6b64c71bad28bdbfb4fab281727923d10a3b123
Sanjarbek <125897994+Berdikulov-571@users.noreply.github.com>
2023-10-27 20:04:05 +0500

    Добавьте файлы проекта.

---
+++ DapperInWebApi/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using DapperInWebApi.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DapperInWebApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllUsers()
+        {
+            return Ok(UserService.GetAll());
+        }
+    }
+}
---
+++ DapperInWebApi/Service/UserService.cs
@@ -0,0 +1,24 @@
+﻿using Dapper;
+using DapperInWebApi.Models;
+using System.Data.SqlClient;
+
+namespace DapperInWebApi.Service
+{
+    public class UserService
+    {
+        public static List<User> GetAll()
+        {
+            string connectionString = WebApplication.CreateBuilder().Configuration.GetConnectionString("DefaultConnection");
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                string query = "select * from userTable";
+
+                List<User> user = connection.Query<User>(query).ToList();
+
+                return user;
+            }
+        }
+
+    }
+}

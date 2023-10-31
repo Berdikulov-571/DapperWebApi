4ff36269dac8da246d81b68644eadb4ef24abeb1
Sanjarbek <125897994+Berdikulov-571@users.noreply.github.com>
2023-10-31 21:38:36 +0500

    StudentControllerCreated

---
+++ DapperInWebApi/Controllers/StudentController.cs
@@ -0,0 +1,32 @@
+﻿using DapperInWebApi.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DapperInWebApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetStudentByID(int studentId)
+        {
+            var res = UserService.GetUserById(studentId);
+            return Ok(res);
+        }
+
+        [HttpGet]
+        public IActionResult GetUsersWithYear(int startDate, int endDate)
+        {
+            var res = UserService.GetUsersWithYear(startDate, endDate);
+            return Ok(res);
+        }
+
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            var res = UserService.GetAll();
+
+            return Ok(res);
+        }
+    }
+}
